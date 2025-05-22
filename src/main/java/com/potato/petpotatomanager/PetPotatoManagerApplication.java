acd7bdfb5b177cfc3788ebb8880851e84fd99c9d
seokjuun <seokjuun@gmail.com>
2025-05-22 09:42:41 +0900

    chore: 초기 세팅

---
+++ src/main/java/com/potato/petpotatomanager/PetPotatoManagerApplication.java
@@ -0,0 +1,13 @@
+package com.potato.petpotatomanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetPotatoManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetPotatoManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/potato/petpotatomanager/PetPotatoManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.potato.petpotatomanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetPotatoManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
